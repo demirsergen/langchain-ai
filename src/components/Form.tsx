137937f41f59c8a1b48077ad40a8227c602820ea
Sergen Demir <dmrsrgn@gmail.com>
2023-04-27 18:06:12 -0500

    update form component and index.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,7 +5,7 @@ const Form = () => {
   const [formData, setFormData] = useState({
     market: '',
     audiance: '',
-    goals: [],
+    goal: '',
   });
 
   const handleChange = (e) => {
@@ -18,39 +18,51 @@ const Form = () => {
     e.preventDefault();
     console.log(formData);
   };
+
+  const formStyles = 'p-2 bg-gray-400 w-1/3 rounded';
+  const inputStyles = 'p-1 text-sm rounded w-2/3';
+  const containerStyles = 'p-1 flex justify-between';
+  const buttonStyles = 'p-1 rounded bg-gray-200 block mx-auto';
   return (
-    <form>
-      <div>
+    <form className={formStyles}>
+      <div className={containerStyles}>
         <label htmlFor="market">Market:</label>
         <input
           type="text"
           id="market"
           name="market"
+          className={inputStyles}
           value={formData.market}
           onChange={handleChange}
         />
       </div>
-      <div>
+      <div className={containerStyles}>
         <label htmlFor="audiance">Audiance:</label>
         <input
           type="text"
           id="audiance"
           name="audiance"
+          className={inputStyles}
           value={formData.audiance}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label htmlFor="goals">Goals:</label>
+      <div className={containerStyles}>
+        <label htmlFor="goal">Goal:</label>
         <input
           type="text"
-          id="goals"
-          name="goals"
-          value={formData.goals}
+          id="goal"
+          name="goal"
+          className={inputStyles}
+          value={formData.goal}
           onChange={handleChange}
         />
       </div>
-      <Button text="Submit" callback={handleSubmit} />
+      <Button
+        text="Submit"
+        callback={handleSubmit}
+        style={buttonStyles}
+      />
     </form>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center gap-2 p-24 ${inter.className}`}
     >
-      <h1>app</h1>
+      <h1>LangChain AI + OpenAI</h1>
       <Form />
     </main>
   );
