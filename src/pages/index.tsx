6bb55a8e68962a50be013d7cced243c25d1e0d40
Sergen Demir <dmrsrgn@gmail.com>
2023-04-29 08:42:05 -0500

    move code to a different file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
 import { Inter } from 'next/font/google';
 import Form from '@/components/Form';
 import Output from '@/components/Output';
-import { OpenAI } from 'langchain/llms/openai';
-import { PromptTemplate } from 'langchain/prompts';
-import { LLMChain } from 'langchain/chains';
+import { chain } from '../../langchain';
 import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [output, setOutput] = useState('');
-  const openAIKey = process.env.NEXT_PUBLIC_OPENAI_KEY || '';
-  const model = new OpenAI({
-    openAIApiKey: openAIKey,
-    temperature: 0.9,
-  });
-  const template =
-    'What is a good name for a company that makes {product}? Do not make it more than 2 words.';
-  const prompt = new PromptTemplate({
-    template: template,
-    inputVariables: ['product'],
-  });
-  const chain = new LLMChain({ llm: model, prompt: prompt });
 
   const getResponse = async (product: string) => {
     const res = await chain.call({ product: product });
