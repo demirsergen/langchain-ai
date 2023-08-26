484397fc5b3ba193173490d3610b40a2cec752d5
Sergen Demir <dmrsrgn@gmail.com>
2023-08-26 10:57:16 -0500

    try to setup langchain agent

--- langchain.js
+++ langchain.js
@@ -1,38 +1,35 @@
-import { OpenAI } from 'langchain/llms/openai';
-import { PromptTemplate } from 'langchain/prompts';
-import { LLMChain } from 'langchain/chains';
 import { initializeAgentExecutorWithOptions } from 'langchain/agents';
+import { OpenAI } from 'langchain/llms/openai';
 import { SerpAPI } from 'langchain/tools';
 import { Calculator } from 'langchain/tools/calculator';
 
-export const run = async () => {
-  const model = new OpenAI({
-    openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-    temperature: 0,
-  });
-  const tools = [
-    new SerpAPI(process.env.NEXT_PUBLIC_SERPAPI_KEY, {
-      location: 'Chicago,Illinois,United States',
-      hl: 'en',
-      gl: 'us',
-    }),
-    new Calculator(),
-  ];
+const model = new OpenAI({
+  openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+  temperature: 0,
+});
+const tools = [
+  new SerpAPI(process.env.NEXT_PUBLIC_SERPAPI_API_KEY, {
+    location: 'Chicago,Illinois,United States',
+    hl: 'en',
+    gl: 'us',
+  }),
+  new Calculator(),
+];
 
-  const executor = await initializeAgentExecutorWithOptions(
-    tools,
-    model,
-    {
-      agentType: 'zero-shot-react-description',
-      verbose: true,
-    }
-  );
-  console.log('Loaded agent.');
+const executor = await initializeAgentExecutorWithOptions(
+  tools,
+  model,
+  {
+    agentType: 'zero-shot-react-description',
+    verbose: true,
+  }
+);
 
-  const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
-  console.log(`Executing with input "${input}"...`);
+const input =
+  'What was the high temperature in SF yesterday in Fahrenheit? What is that number raised to the .023 power?';
 
-  const result = await executor.call({ input });
+const result = await executor.call({
+  input,
+});
 
-  console.log(`Got output ${result.output}`);
-};
+console.log(result);
